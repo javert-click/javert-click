97c589ac934f44d29af0063cd5d149f265148fa7
Gabisampaio <g.sampaio17@imperial.ac.uk>
2021-05-05 16:50:42 +0100

    few changes to fibonacci example

--- src/js/Examples/WebWorkers/symbolic/Fibonacci/MainFibonacci.js
+++ src/js/Examples/WebWorkers/symbolic/Fibonacci/MainFibonacci.js
@@ -3,10 +3,9 @@ const Worker = require('../../../../MessagePassing/WebWorkers/Worker');
 console.log('MAIN: Going to initiliaze heap!');
 console.log('MAIN: Heap initialized!');
 
-//var n = 6;
 var n = symb_number(n);
-//var n_constraint = (n == 1);
-JavertAssume(n = 6);
+var n_constraint = (n == 1 || n == 2);
+JavertAssume(n_constraint);
 var worker = new Worker.Worker('FibonacciWorker.js');
 console.log('MAIN: worker created with id '+worker.__id);
 console.log('MAIN: going to send message to worker');
@@ -14,6 +13,6 @@ worker.postMessage(n);
 console.log('MAIN: message sent to worker');
 worker.onmessage = function(e) { 
     var res = e.data;
-    JavertAssert (res = 8);
+    JavertAssert (res = 1);
     console.log('MAIN: got result from worker:'+e.data);
 };
