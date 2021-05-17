a8b65ffc3053bf6d69a982ec628a27e3e10e8fb9
Gabisampaio <g.sampaio17@imperial.ac.uk>
2021-05-17 16:53:43 +0100

    one more test

---
+++ src/TestSuites/PostMessage/with-options/no-target-origin.js
@@ -0,0 +1,14 @@
+// Title: no targetOrigin</title>
+
+import { assert_equals, async_test } from '../../../js/DOM/Events/Testharness';
+const Window = require('../../../js/DOM/Events/Window');
+var window = Window.getInstance();
+
+async_test(function() {
+  window.postMessage('', {});
+  window.onmessage = this.step_func(function(e) {
+    console.log('Got message, data: '+e.data);
+    assert_equals(e.data, '');
+    this.done();
+  });
+});
