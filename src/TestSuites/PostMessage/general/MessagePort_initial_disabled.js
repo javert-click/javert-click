808648a16c37295ae5916efade3293402ece31d8
Gabisampaio <g.sampaio17@imperial.ac.uk>
2021-06-09 16:54:54 +0100

    Adding one more conc test: MessagePort_initial_disabled.js

---
+++ src/TestSuites/PostMessage/general/MessagePort_initial_disabled.js
@@ -0,0 +1,13 @@
+//Title: MessageChannel: port message queue is initially disabled
+
+import { MessageChannel } from '../../../js/MessagePassing/PostMessage/MessageChannel';
+import { async_test } from '../../../js/DOM/Events/Testharness';
+
+async_test(function(t) {
+    var channel = new MessageChannel();
+    channel.port2.addEventListener("message", t.unreached_func(), true);
+    console.log('Going to call postMessage');
+    channel.port1.postMessage("ping");
+    setTimeout(t.step_func_done(), 100);
+    console.log('Finished test');
+});
