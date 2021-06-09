05c4d52326553e82cffeb0eddd1fe2888dd29e21
Gabisampaio <g.sampaio17@imperial.ac.uk>
2021-06-09 13:24:26 +0100

    Channel_postMessage_target_source.js

---
+++ src/TestSuites/PostMessage/general/Channel_postMessage_target_source.js
@@ -0,0 +1,29 @@
+// Title: postMessage(): target port and source port
+
+import { MessageChannel } from '../../../js/MessagePassing/PostMessage/MessageChannel';
+import { async_test, assert_equals, assert_not_equals } from '../../../js/DOM/Events/Testharness';
+
+var TARGET = null;
+var SOURCE = null;
+var description = "The postMessage() method - Let target port be the port with which source "
+                + "port is entangled, if any.";
+
+var t = async_test("Test Description: " + description);
+
+var channel = new MessageChannel();
+SOURCE = channel.port1;
+TARGET = channel.port2;
+TARGET.start();
+TARGET.addEventListener("message", t.step_func(TestMessageEvent), true);
+
+SOURCE.postMessage("ping");
+
+function TestMessageEvent(evt)
+{
+    console.log('Going to test if evt.target === target? '+(evt.target === TARGET));
+    assert_equals(evt.target, TARGET);
+    console.log('Going to test if evt.target !== SOURCE? '+(evt.target !== SOURCE));
+    assert_not_equals(evt.target, SOURCE);
+    console.log('TEST DONE');
+    t.done();
+}
