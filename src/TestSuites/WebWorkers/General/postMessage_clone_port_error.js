9d17d555011ce0d51da6a676598a87fc136f3ca7
Gabisampaio <g.sampaio17@imperial.ac.uk>
2021-06-10 13:25:10 +0100

    adding workers concrete test: postMessage_clone_port_error.js

---
+++ src/TestSuites/WebWorkers/General/postMessage_clone_port_error.js
@@ -0,0 +1,14 @@
+//Title: postMessage(): cloning source port
+
+import { MessageChannel } from '../../../js/MessagePassing/PostMessage/MessageChannel';
+import { test, assert_throws_dom } from '../../../js/DOM/Events/Testharness';
+
+test(function() {
+    var channel = new MessageChannel();
+    channel.port1.start();
+    console.log('Going to call assert_throws_dom');
+    assert_throws_dom("DataCloneError", function() {
+      channel.port1.postMessage("ports", [channel.port1]);
+    });
+    console.log('Finished test');
+});
