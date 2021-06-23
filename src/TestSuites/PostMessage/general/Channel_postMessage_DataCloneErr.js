2bc8c8ab734f52f7c1519e311ed93b1ce2353291
Gabisampaio <g.sampaio17@imperial.ac.uk>
2021-06-23 20:28:06 +0100

    posting navigator obj raises dom data clone err

---
+++ src/TestSuites/PostMessage/general/Channel_postMessage_DataCloneErr.js
@@ -0,0 +1,16 @@
+//Title: postMessage() with a host object raises DataCloneError
+
+import { MessageChannel } from '../../../js/MessagePassing/PostMessage/MessageChannel';
+import { test, assert_throws_dom } from '../../../js/DOM/Events/Testharness';
+
+var description = "Throw a DataCloneError when a host object (e.g. a DOM node) is used with postMessage.";
+
+test(function()
+{
+    var channel = new MessageChannel();
+    channel.port1.start();
+    assert_throws_dom("DATA_CLONE_ERR", function()
+    {
+        channel.port1.postMessage(navigator);
+    });
+}, description);
