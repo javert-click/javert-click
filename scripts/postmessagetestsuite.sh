6773841ab0969ec402363153255b7f13b5b24739
Gabisampaio <g.sampaio17@imperial.ac.uk>
2021-07-19 11:31:48 +0100

    Fixing script

--- scripts/postmessagetestsuite.sh
+++ scripts/postmessagetestsuite.sh
@@ -6,23 +6,27 @@ declare log_test_file="log_test.log"
 declare passed=0
 declare failed=0
 
+declare workersparam=$2
+
+echo "workersparam?$workersparam"
+
 #Running tests for postMessage
 for testfile in $tests; do
-  ./workersconcrete.sh $testfile $2
+  ./workersconcrete.sh $testfile $workersparam
 
   declare nasserts=`grep -c "TestHarnessAssert.*: 0" $log_test_file`
   declare nasserts_passed=`grep -c "CMD: return" $log_test_file`
   declare nasserts_failed=`grep -c "CMD: throw" $log_test_file`
-  echo "NUMBER OF ASSERTS CHECKED: $nasserts"
-  echo "--Passing: $nasserts_passed"
-  echo "--Failing: $nasserts_failed"
+  #echo "NUMBER OF ASSERTS CHECKED: $nasserts"
+  #echo "--Passing: $nasserts_passed"
+  #echo "--Failing: $nasserts_failed"
   if [[ $nasserts_failed = 0 ]] 
   then
     passed=$(expr $passed + 1)
-    echo "TEST PASSED"
+    #echo "TEST PASSED"
   else 
     failed=$(expr $failed + 1)
-    echo "TEST FAILED"
+    #echo "TEST FAILED"
   fi
 done
 
